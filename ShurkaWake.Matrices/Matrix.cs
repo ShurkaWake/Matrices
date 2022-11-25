fb4653a9988b53072bae57fe467a0030a471ef77
ShurkaWake <a.a.prosud@gmail.com>
2022-11-25 10:54:53 +0200

    Changed basis and sequental add

--- ShurkaWake.Matrices/Matrix.cs
+++ ShurkaWake.Matrices/Matrix.cs
@@ -2,9 +2,10 @@
 
 namespace ShurkaWake.Matrices
 {
-    public class Matrix<T> : IMatrix<T> where T : INumber<T>
+    public class Matrix<T> : IMatrix<T> where T : struct, INumber<T>
     {
-        readonly T[,] _values;
+        readonly int chunkSize = Vector<T>.Count;
+        readonly Vector<T>[,] _values;
         readonly int _length;
 
         public Matrix(int n)
@@ -14,8 +15,16 @@ namespace ShurkaWake.Matrices
                 throw new ArgumentException("n must be higher than zero");
             }
 
-            _values = new T[n, n];
             _length = n;
+            _values = new Vector<T>[n, (int) Math.Ceiling(n / (double) chunkSize)];
+
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < _values.GetLength(1); j++)
+                {
+                    _values[i, j] = new Vector<T>(new T[chunkSize]);
+                }
+            }
         }
 
         public Matrix(T[,] values)
@@ -25,11 +34,31 @@ namespace ShurkaWake.Matrices
                 throw new ArgumentException("Matrix must have same dimensions");
             }
 
-            _values = values;
-            _length = Values.GetLength(0);
+            _length = values.GetLength(0);
+            _values = new Vector<T>[Length, (int)Math.Ceiling(Length / (double)chunkSize)];
+
+            for (int i = 0; i < Length; i++)
+            {
+                for (int j = 0; j < _values.GetLength(1); j++)
+                {
+                    var arr = new T[chunkSize];
+                    for (int k = 0; k < chunkSize; k++)
+                    {
+                        if(IsInBounds(i, j * chunkSize + k))
+                        {
+                            arr[k] = values[i, j * chunkSize + k];
+                        }
+                        else
+                        {
+                            arr[k] = default(T);
+                        }
+                    }
+                    _values[i, j] = new Vector<T>(arr);
+                }
+            }
         }
 
-        private T[,] Values => _values;
+        private Vector<T>[,] Values => _values;
         public int Length => _length;
 
         public T this[int x, int y]
@@ -37,121 +66,78 @@ namespace ShurkaWake.Matrices
             get
             {
                 ThrowExceptionIfNotInBounds(x, y);
-                return _values[x, y];
+                return Values[x, y / chunkSize][y % chunkSize];
             }
             set
             {
                 ThrowExceptionIfNotInBounds(x, y);
-                _values[x, y] = value;
+                T[] temp = new T[chunkSize];
+                for (int i = 0; i < chunkSize; i++)
+                {
+                    temp[i] = this[x, y / chunkSize + i];
+                }
+                temp[y % chunkSize] = value;
+                _values[x, y / chunkSize] = new Vector<T>(temp);
             }
         }
 
         public IMatrix<T> SequentalAdd(IMatrix<T> matrix)
         {
             ThrowExceptionIfNotEqualLength(matrix);
+            Matrix<T> result = new Matrix<T>(Length);
 
-            Matrix<T> result = new Matrix<T>(this.Length);
-            for (int i = 0; i < Length; i++)
+            if (matrix is Matrix<T> second)
             {
-                for (int j = 0; j < Length; j++)
+                for (int i = 0; i < Length; i++)
                 {
-                    result[i, j] = this[i, j] + matrix[i, j];
+                    for (int j = 0; j < Values.GetLength(1); j++)
+                    {
+                        result.Values[i, j] = Values[i, j] + second.Values[i, j];
+                    }
                 }
             }
+            else
+            {
+                for (int i = 0; i < Length; i++)
+                {
+                    for (int j = 0; j < Length; j++)
+                    {
+                        result[i, j] = this[i, j] + matrix[i, j];
+                    }
+                }
+            }
+
             return result;
         }
 
         public IMatrix<T> ParallelAdd(IMatrix<T> matrix)
         {
-            ThrowExceptionIfNotEqualLength(matrix);
+            throw new NotImplementedException();
 
-            Matrix<T> result = new Matrix<T>(Length);
-            Parallel.For(0, Length, (i) =>
-            {
-                for (int j = 0; j < Length; j++)
-                {
-                    result[i, j] = this[i, j] + matrix[i, j];
-                }
-            });
-
-            return result;
         }
 
 
         public IMatrix<T> ParallelAdd(IMatrix<T> matrix, int threads)
         {
-            ThrowExceptionIfNotEqualLength(matrix);
-            ParallelOptions parallelOptions = new ParallelOptions();
-            parallelOptions.MaxDegreeOfParallelism = threads;
+            throw new NotImplementedException();
 
-            Matrix<T> result = new Matrix<T>(Length);
-            Parallel.For(0, Length, parallelOptions, (i) =>
-            {
-                for (int j = 0; j < Length; j++)
-                {
-                    result[i, j] = this[i, j] + matrix[i, j];
-                }
-            });
-
-            return result;
         }
 
         public IMatrix<T> SequentalMultiply(IMatrix<T> matrix)
         {
-            ThrowExceptionIfNotEqualLength(matrix);
+            throw new NotImplementedException();
 
-            Matrix<T> result = new Matrix<T>(Length);
-            for (int i = 0; i < Length; i++)
-            {
-                for (int j = 0; j < Length; j++)
-                {
-                    for (int k = 0; k < Length; k++)
-                    {
-                        result[i, j] += this[i, k] * matrix[k, j];
-                    }
-                }
-            }
-            return result;
         }
 
         public IMatrix<T> ParallelMultiply(IMatrix<T> matrix)
         {
-            ThrowExceptionIfNotEqualLength(matrix);
+            throw new NotImplementedException();
 
-            Matrix<T> result = new Matrix<T>(Length);
-            Parallel.For(0, Length, (i) =>
-            {
-                for (int j = 0; j < Length; j++)
-                {
-                    for (int k = 0; k < Length; k++)
-                    {
-                        result[i, j] += this[i, k] * matrix[k, j];
-                    }
-                }
-            });
-
-            return result;
         }
 
         public IMatrix<T> ParallelMultiply(IMatrix<T> matrix, int threads)
         {
-            ThrowExceptionIfNotEqualLength(matrix);
-            ParallelOptions parallelOptions = new ParallelOptions();
-            parallelOptions.MaxDegreeOfParallelism = threads;
-
-            Matrix<T> result = new Matrix<T>(Length);
-            Parallel.For(0, Length, parallelOptions, (i) =>
-            {
-                for (int j = 0; j < Length; j++)
-                {
-                    for (int k = 0; k < Length; k++)
-                    {
-                        result[i, j] += this[i, k] * matrix[k, j];
-                    }
-                }
-            });
-
-            return result;
+            throw new NotImplementedException();
         }
 
         private bool IsInBounds(int index)
